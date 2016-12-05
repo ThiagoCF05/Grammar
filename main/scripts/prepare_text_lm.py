b6589312cf2169ddce6b1ec2ffe483f86dea0bec
Thiago Castro Ferreira <thiago.castro.ferreira@gmail.com>
2016-12-05 14:32:16 -0200

    Script to prepare text for LM training

---
+++ main/scripts/prepare_text_lm.py
@@ -0,0 +1,26 @@
+__author__ = 'thiagocastroferreira'
+
+from sys import path
+path.append('/home/tcastrof/amr/Grammar')
+import os
+import main.utils as utils
+
+def run():
+    dirs = ['../data/LDC2015E86/data/amrs/split/training',
+            '../data/LDC2015E86/data/amrs/split/dev',
+            '../data/LDC2016E25/data/amrs/split/training',
+            '../data/LDC2016E25/data/amrs/split/dev',
+            '../data/LDC2016E33/data/amrs']
+
+    texts = []
+    for dir in dirs:
+        for fname in os.listdir(dir):
+            print fname, '\r',
+            amrs = utils.parse_corpus(os.path.join(dir, fname))
+
+            for amr in amrs:
+                texts.append(amr['sentence'])
+
+    f = open('text_for_lm')
+    f.write('\n'.join(texts))
+    f.close()
