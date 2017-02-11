a8d7ca6d1d7b521c45e67485040d804ddb5768b0
Thiago Castro Ferreira <thiago.castro.ferreira@gmail.com>
2017-02-11 11:35:09 -0200

    gigaword

---
+++ main/scripts/prepare_gigaword_lm.py
@@ -0,0 +1,59 @@
+import gzip
+import nltk
+import os
+
+import xml.etree.ElementTree as ET
+
+def process_text(xml, data):
+    root = ET.fromstring(xml)
+
+    t = root.find('TEXT')
+
+    headline = t.find('HEADLINE')
+    text = str(headline.text).replace('\n', ' ').lower()
+    text = ' '.join(nltk.word_tokenize(text)) + '\n'
+    data = data + text
+
+    paragraphs = t.findall('P')
+    for paragraph in paragraphs:
+        text = str(paragraph.text).replace('\n', ' ').lower()
+        text = ' '.join(nltk.word_tokenize(text)) + '\n'
+        data = data + text
+
+def process(fname, data, ndoc, nerror):
+    f = gzip.open(fname)
+    doc = f.read()
+    f.close()
+
+    xml = ''
+    for line in doc.split('\n'):
+        xml = xml + line + '\n'
+        if line.strip() == '</DOC>':
+            try:
+                data = process_text(xml, data)
+                xml = ''
+                ndoc = ndoc + 1
+            except:
+                nerror = nerror + 1
+    return data, ndoc, nerror
+
+
+if __name__ == '__main__':
+    dirs = ['/roaming/tcastrof/gigaword/LDC2007T07/gigaword_eng_3a/data/afp_eng']
+            # '/roaming/tcastrof/gigaword/LDC2007T07/gigaword_eng_3a/data/apw_eng',
+            # '/roaming/tcastrof/gigaword/LDC2007T07/gigaword_eng_3a/data/cna_eng'
+            # '/roaming/tcastrof/gigaword/LDC2007T07/gigaword_eng_3b/data/ltw_eng',
+            # '/roaming/tcastrof/gigaword/LDC2007T07/gigaword_eng_3b/data/nyt_eng',
+            # '/roaming/tcastrof/gigaword/LDC2007T07/gigaword_eng_3b/data/xin_eng']
+
+    data, ndoc, nerror = '', 0, 0
+    for dir in dirs:
+        for fname in os.listdir(dir):
+            data, ndoc, nerror =  process(fname, data, ndoc, nerror)
+
+            print 'PROCESSED: ', ndoc, '/ ERRORS: ', nerror
+
+    fwrite = '/roaming/tcastrof/gigaword/training.txt'
+    f = open(fwrite, 'w')
+    f.write(data)
+    f.close()
