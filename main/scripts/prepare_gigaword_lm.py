92f99b0c68a0579a884cfb4fc387b1f0e7541b7c
Thiago Castro Ferreira <thiago.castro.ferreira@gmail.com>
2017-02-11 18:51:17 -0200

    apw_eng

--- main/scripts/prepare_gigaword_lm.py
+++ main/scripts/prepare_gigaword_lm.py
@@ -40,8 +40,8 @@ def process(fname, data, ndoc, nerror):
     return data, ndoc, nerror
 
 if __name__ == '__main__':
-    dirs = ['/roaming/tcastrof/gigaword/LDC2007T07/gigaword_eng_3a/data/afp_eng']
-            # '/roaming/tcastrof/gigaword/LDC2007T07/gigaword_eng_3a/data/apw_eng',
+    dirs = [#'/roaming/tcastrof/gigaword/LDC2007T07/gigaword_eng_3a/data/afp_eng']
+             '/roaming/tcastrof/gigaword/LDC2007T07/gigaword_eng_3a/data/apw_eng']
             # '/roaming/tcastrof/gigaword/LDC2007T07/gigaword_eng_3a/data/cna_eng'
             # '/roaming/tcastrof/gigaword/LDC2007T07/gigaword_eng_3b/data/ltw_eng',
             # '/roaming/tcastrof/gigaword/LDC2007T07/gigaword_eng_3b/data/nyt_eng',
@@ -58,7 +58,7 @@ if __name__ == '__main__':
 
             print '\nPROCESSED: ', ndoc, '/ ERRORS: ', nerror
 
-    fwrite = '/roaming/tcastrof/gigaword/training.txt'
+    fwrite = '/roaming/tcastrof/gigaword/apw_eng.txt'
     f = open(fwrite, 'w')
     f.write(data.encode('utf-8'))
     f.close()
